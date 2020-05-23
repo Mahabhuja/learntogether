558bb9288091a24bb3261d0d9c6ce5512cb6a7ef
Ravysh <rkt1709@gmail.com>
2020-05-23 18:34:25 +0530

    practice problems

--- src/main/java/com/rkt/learntogether/practice/Practice.java
+++ src/main/java/com/rkt/learntogether/practice/Practice.java
@@ -3,7 +3,8 @@ package com.rkt.learntogether.practice;
 public class Practice {
 
     public static void main(String[] args) {
-        draw(5);
+        //draw(5);
+        System.out.println(isLucky(1230));
     }
 
     static void draw(int i) {
@@ -15,5 +16,20 @@ public class Practice {
             System.out.print("#");
         }
         System.out.println("");
+    }
+
+    static boolean isLucky(int n) {
+        String s = Integer.toString(n);
+        int l = s.length();
+        String s1 = s.substring(0, (l/2));
+        String s2 = s.substring(l/2);
+        return getSum(s1) == getSum(s2);
+    }
+    static int getSum(String s) {
+        int result = 0;
+        for (String a : s.split("")) {
+            result = result + Integer.parseInt(a);
+        }
+        return result;
     }
 }
